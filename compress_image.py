3353a43d881ee756ac5f8234f2dca070d036f3f1
内藤秀彦 <hidehiko@7110.email>
2017-08-08 23:15:32 +0900

    update to speed up
    close buffer at end of each roop,
    use dichotomy for quality rate,
    fix maximum of quality rate to 95

--- compress_image.py
+++ compress_image.py
@@ -5,6 +5,20 @@ from io import BytesIO
 from PIL import Image
 
 
+def compress(image, rate):
+    buffer = BytesIO()
+    image.save(buffer, "JPEG", quality=(95-rate))
+
+    if len(buffer.getvalue()) < file_size:
+        with open(new_img_name,"wb") as f:
+            f.write(buffer.getvalue())
+        return True
+    else:
+        return False
+
+    buffer.close()
+
+
 argvs = sys.argv
 
 if len(argvs) == 3:
@@ -18,16 +32,26 @@ if len(argvs) == 3:
     splited_name =img_name.split(".")
     new_img_name = splited_name[0]+"_compressed."+".".join(splited_name[1:])
 
-    for i in range(100):
-        buffer = BytesIO()
-        img.save(buffer, "JPEG", quality=(100-i))
+    # It can be compressed to specified size or less
+    if compress(img, 94):
 
-        if len(buffer.getvalue()) < file_size:
-            with open(new_img_name,"wb") as f:
-                f.write(buffer.getvalue())
-            break
-        elif i >= 99:
-            print("Error: can not compress to the specified size")
+        # compression ratio: 0 -> 47
+        if compress(img, 47):
+            for i in range(48):
+                if compress(img, i):
+                    print("Succeed!, compression rate: {}".format(95-i))
+                    break
+
+        # compression ratio: 48 -> 95
+        else:
+            for i in range(47):
+                if compress(img, i+48):
+                    print("Succeed!, compression rate: {}".format(95-48-i))
+                    break
+
+    # impossible
+    else:
+        print("Error: can not compress to the specified size")
 
 else:
     print("Error: input error")
